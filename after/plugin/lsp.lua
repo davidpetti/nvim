e2fa6af2a6d7b2978d480f2983a175ec06a1bc03
davidpetti <pettid412@gmail.com>
2023-09-09 12:59:36 -0400

    init

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,36 @@
+local lsp = require('lsp-zero').preset({})
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').gopls.setup({
+	capabilities = lsp_capabilities,
+	on_attach = function(client, bufnr)
+	lsp_zero.default_keymaps({buffer = bufnr})
+  end,
+})
+require('lspconfig').tsserver.setup({
+  capabilities = lsp_capabilities,
+  on_attach = function(client, bufnr)
+    lsp_zero.default_keymaps({buffer = bufnr})
+  end,
+})
+require('lspconfig').pyright.setup({
+  capabilities = lsp_capabilities,
+  on_attach = function(client, bufnr)
+    lsp_zero.default_keymaps({buffer = bufnr})
+  end,
+})
+require('lspconfig').java_language_server.setup({
+  capabilities = lsp_capabilities,
+  on_attach = function(client, bufnr)
+    lsp_zero.default_keymaps({buffer = bufnr})
+  end,
+})
+
+lsp.setup()
